5b89bf42e7e525d77f022b7cf8262c6cc28b5c08
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-11-17 15:46:12 +0100

    switch ci

---
+++ hydra/jobsets.nix
@@ -0,0 +1,90 @@
+{
+  prsJSON,
+  nixpkgs,
+}: let
+  pkgs = import nixpkgs {};
+  prs = builtins.fromJSON (builtins.readFile prsJSON);
+  jobsets =
+    (builtins.listToAttrs (
+      pkgs.lib.attrsets.mapAttrsToList (
+        _: info: {
+          name = "pr${toString info.number}";
+          value = {
+            enabled = info.state == "open";
+            hidden = info.state != "open";
+            description = "PR ${toString info.number}: ${info.title}";
+            nixexprinput = "admin-fe";
+            nixexprpath = "hydra/default.nix";
+            checkinterval = 3600;
+            schedulingshares = 100;
+            enableemail = false;
+            emailoverride = "";
+            keepnr = 1;
+            inputs = {
+              admin-fe = {
+                type = "git";
+                value = "${info.head.repo.clone_url} ${info.head.ref}";
+                emailresponsible = false;
+              };
+              nixpkgs = {
+                type = "git";
+                value = "https://github.com/NixOS/nixpkgs.git master";
+                emailresponsible = false;
+              };
+              github_input = {
+                type = "string";
+                value = "admin-fe";
+              };
+              github_repo_owner = {
+                type = "string";
+                value = info.head.repo.owner.login;
+              };
+              github_repo_name = {
+                type = "string";
+                value = info.head.repo.name;
+              };
+            };
+          };
+        }
+      )
+      prs
+    ))
+    // {
+      admin-fe = {
+        enabled = 1;
+        hidden = false;
+        description = "Akkoma admin-fe";
+        nixexprinput = "admin-fe";
+        nixexprpath = "hydra/default.nix";
+        checkinterval = 0;
+        schedulingshares = 100;
+        enableemail = false;
+        emailoverride = "";
+        keepnr = 1;
+        inputs = {
+          admin-fe = {
+            type = "git";
+            value = "https://github.com/DarkKirb/admin-fe develop";
+            emailresponsible = false;
+          };
+          nixpkgs = {
+            type = "git";
+            value = "https://github.com/NixOS/nixpkgs.git master";
+            emailresponsible = false;
+          };
+          github_input = {
+            type = "string";
+            value = "admin-fe";
+          };
+          github_repo_owner = {
+            type = "string";
+            value = "DarkKirb";
+          };
+          github_repo_name = {
+            type = "string";
+            value = "admin-fe";
+          };
+        };
+      };
+    };
+in {jobsets = pkgs.writeText "jobsets.json" (builtins.toJSON jobsets);}
